308dd450bd7be52ae870f34f3df9033a21b0fb2d
jairvagn <jalovargon@gmail.com>
2021-06-26 12:41:29 -0500

    agregadas las clases entidad configuracion definida para rol, error en persistencia strategy

---
+++ src/main/java/entities/Rol.java
@@ -0,0 +1,64 @@
+package entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+@Entity
+@Table(name="ROL")
+
+public class Rol implements Serializable {
+
+    private static final long serialVersionUID = 6362794385792247263L;
+
+    private long id;
+    private  String descripcion;
+    private String  siglaRol;
+    private boolean estado;
+
+    public Rol(){
+
+    }
+
+    @Id
+    @-( strategy = GenerationType.IDENTITY )
+    @Column(name="ID_ROL_PK", nullable = false)
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Column(name="DESCRIPCION", nullable = false)
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Column(name ="SIGLA_ROL")
+    public String getSiglaRol() {
+        return siglaRol;
+    }
+
+    public void setSiglaRol(String siglaRol) {
+        this.siglaRol = siglaRol;
+    }
+
+    @Column(name="ESTADO", nullable = false)
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+}
+
+
