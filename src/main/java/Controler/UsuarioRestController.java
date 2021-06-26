d500552d6cb0f559b7aac7dc23bb5e1381b81a8e
jairvagn <jalovargon@gmail.com>
2021-06-26 13:54:49 -0500

    Se agrega la clase rest controler

---
+++ src/main/java/Controler/UsuarioRestController.java
@@ -0,0 +1,27 @@
+package Controler;
+
+import entities.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import services.IUsuarioServices;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class UsuarioRestController {
+
+    @Autowired
+    private IUsuarioServices usuarioServices;
+
+    @GetMapping("/usuario")
+    public List<Usuario> index() {
+      return usuarioServices.findAll();
+    }
+
+    @PostMapping("/usuario")
+    public  Usuario create(@RequestBody Usuario usuario){
+        return usuarioServices.create(usuario);
+    }
+
+}
