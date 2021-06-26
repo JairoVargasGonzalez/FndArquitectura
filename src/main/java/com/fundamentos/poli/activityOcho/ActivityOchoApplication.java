ee2b7972ced300d0351c2cd6cc81dbd2163ba239
jairvagn <jalovargon@gmail.com>
2021-06-26 12:08:33 -0500

    Inicialización del proyecto

---
+++ src/main/java/com/fundamentos/poli/activityOcho/ActivityOchoApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentos.poli.activityOcho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityOchoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivityOchoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentos/poli/activityOcho/ActivityOchoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentos.poli.activityOcho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityOchoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
